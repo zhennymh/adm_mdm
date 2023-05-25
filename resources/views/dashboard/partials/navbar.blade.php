d072b853fda058221fd1d7e5f0a62228c08c73d1
Mahisa Ajy Kusuma <mahisaajy@gmail.com>
2023-05-25 13:15:01 +0700

    tambah visualisasi penginput data phk.

--- resources/views/dashboard/partials/navbar.blade.php
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -73,6 +73,9 @@
                         <a class="dropdown-item" href="http://172.19.2.189/views/DataPosHujan/DashboardData9999?iframeSizedToWindow=true&:embed=y&:showAppBanner=false&:display_count=no&:showVizHome=no&:origin=viz_share_link" target="_blank">
                           Data Hujan 9999 Pos Hujan Kerjasama 
                         </a>
+                        <a class="dropdown-item" href="http://172.19.2.189/views/DataPosHujan/DashboardPenginputDataPHK?iframeSizedToWindow=true&:embed=y&:showAppBanner=false&:display_count=no&:showVizHome=no&:origin=viz_share_link" target="_blank">
+                          Penginput Data Pos Hujan Kerjasama
+                        </a>
                         
                         <!-- <a class="dropdown-item text-pink" href="https://github.com/sponsors/codecalm" target="_blank" rel="noopener">                        
                           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-inline me-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
